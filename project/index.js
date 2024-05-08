2983471b9ad9810eaa7dac617dee0a1d0848b5af
Harshad Katariya <159020392+HarshadKatariya007@users.noreply.github.com>
2024-05-08 16:29:15 +0530

    Node Js Project

---
+++ project/index.js
@@ -0,0 +1,34 @@
+const fs  = require('fs');
+
+const HTTP =  require('http');
+let Server= HTTP.createServer((req,res) =>
+{
+   if(req.url=="/")
+   {
+        fs.readFile('./index.html',(err,data) =>
+        {
+            if(err) throw new err
+            res.end(data)  
+        })
+   }
+   else if(req.url=="/login")
+   {
+        res.end("Welcome To Login")
+   }
+   else if(req.url=="/signup")
+   {
+        res.end("Welcome To Signup")
+   }
+   else if(req.url=="/contact")
+    {
+        res.end("Welcome To Contact")
+    }
+    else if(req.url=="/service")
+    {
+        res.end("Welcome To Service")
+    }            
+})
+Server.listen(8090,() =>
+{
+    console.log("Server Is Running");
+})
